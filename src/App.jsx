1e63af743ed004bcec6bf0211e2de4af5487f529
ChandraRaditya <chandraariwiraditya@gmail.com>
2022-07-18 21:59:57 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello everyone</h1>
+      <p>This is first page</p>
+    </div>
+  );
+}
+
+export default App;
