85d2f6a2e6badbe3a8bac6d0939d7df63e8e2d81
ChandraRaditya <chandraariwiraditya@gmail.com>
2022-07-20 00:21:28 +0700

    add: bottom section on uppercontent

--- src/Component/UpperContent/index.jsx
+++ src/Component/UpperContent/index.jsx
@@ -1,6 +1,8 @@
 import "./index.scss";
 import Bg from "../../Assets/BG.svg";
+import oval from "../../Assets/Oval.svg";
 import cosmonot from "../../Assets/cosmonot.png";
+import cosmonotLeft from "../../Assets/cosmonot-left.png";
 
 function UpperContent() {
   return (
@@ -17,7 +19,26 @@ function UpperContent() {
           />
         </div>
       </div>
-      <div className="bottom-section"></div>
+      <div className="bottom-section">
+        <div className="bottom-section__cosmonot-left-decoration-container">
+          <img
+            className="bottom-section__cosmonot-left-decoration"
+            src={cosmonotLeft}
+            alt=""
+          />
+        </div>
+        <p className="bottom-section__text">
+          <span className="bottom-section__text_blue">Definition</span>; a
+          practice or exercise to test or improve one's fitness for athletic
+          competition, ability, or performance to exhaust (something, such as a
+          mine) by working to devise, arrange, or achieve by resolving
+          difficulties. <br /> Merriam-Webster.com Dictionary. <br />
+          <span className="bottom-section__text_white">-weekend team</span>
+        </p>
+        <div className="bottom-section__oval-decoration-container">
+          <img className="bottom-section__oval-decoration" src={oval} alt="" />
+        </div>
+      </div>
     </div>
   );
 }
