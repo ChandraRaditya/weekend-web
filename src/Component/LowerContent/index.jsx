2b142891ab5cefcf79896d32649a18227e14242e
ChandraRaditya <chandraariwiraditya@gmail.com>
2022-07-21 19:18:33 +0700

    add: padding on card testimonial

--- src/Component/LowerContent/index.jsx
+++ src/Component/LowerContent/index.jsx
@@ -1,4 +1,5 @@
 import "./index.scss";
+import cosmonotBottom from "../../Assets/cosmonot-bottom.png";
 
 function LowerContent() {
   return (
@@ -28,14 +29,21 @@ function LowerContent() {
       <div className="lower-content__section tips-section">
         <div className="tips-section__container"></div>
       </div>
-      <div className="lower-content__section text-section">
-        <div className="text-section__container">
-          <h2 className="text-section__title">You’re all set.</h2>
-          <p className="text-section__text">
-            The wise man therefore always holds in these matters to this
-            principle of selection.
-          </p>
+      <div className="lower-content__bottom-part">
+        <div className="lower-content__section text-section">
+          <div className="text-section__container">
+            <h2 className="text-section__title">You’re all set.</h2>
+            <p className="text-section__text">
+              The wise man therefore always holds in these matters to this
+              principle of selection.
+            </p>
+          </div>
         </div>
+        <img
+          className="lower-content__image-decoration"
+          src={cosmonotBottom}
+          alt=""
+        />
       </div>
     </div>
   );
